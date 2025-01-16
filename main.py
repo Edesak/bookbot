83b98e063fd989374e418903e66d8cc6126f1a67
Edesak <siegel.marek@gmail.com>
2025-01-16 21:04:19 +0100

    Clean up and commneting added

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 def main():
+    #Path to book
     path = "books/frankenstein.txt"
     text = get_text(path)
+    #creating new variable cause of keeping the original untouched
     text_lowered = text.lower()
     number_of_words = count_words(text)
     u_char_count = count_unique(text_lowered)
     sorted_dict = sort_dictionary(u_char_count)
-    print(sorted_dict)
     report(path,number_of_words,sorted_dict)
 
+#cheking if character is in aplhabet and returning clean dictionary 
 def clean_dictionary(dic):
     clean_dic = {}
     for d in dic:
-        #print(d)
         if d.isalpha():
             clean_dic[d] = dic[d]
             
     return clean_dic
 
+#sorting dictionary with cleaning up for better performace
 def sort_dictionary(dic):
     dic = clean_dictionary(dic)
+    #getting dictionary to list
     list = dic.items()
+    #swaping places in tuple 
     swapped_list = [(a,b) for b,a in list]
+    #sort the list then creating dictionary and return it
     return dict(sorted(swapped_list,reverse=True))
 
+#counting amout of words
 def count_words(s):
     s = s.split()
     return len(s)
 
+#Converting file to string
 def get_text(path):
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return(file_contents)
 
 def count_unique(s):
-    characters = set(s)
     character_count = {}
     for char in s:
         if char in character_count:
