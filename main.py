8a56d41b6634a010e4a64ca099522a6683f2ba83
Edesak <siegel.marek@gmail.com>
2025-01-16 20:57:43 +0100

    Added sorting and cleaning the dictionary with some pretty output

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def main():
     path = "books/frankenstein.txt"
     text = get_text(path)
-    number_of_words = count_words(text)
     text_lowered = text.lower()
-    print(number_of_words)
+    number_of_words = count_words(text)
+    u_char_count = count_unique(text_lowered)
+    sorted_dict = sort_dictionary(u_char_count)
+    print(sorted_dict)
+    report(path,number_of_words,sorted_dict)
 
-    count_unique(text_lowered)
+def clean_dictionary(dic):
+    clean_dic = {}
+    for d in dic:
+        #print(d)
+        if d.isalpha():
+            clean_dic[d] = dic[d]
+            
+    return clean_dic
+
+def sort_dictionary(dic):
+    dic = clean_dictionary(dic)
+    list = dic.items()
+    swapped_list = [(a,b) for b,a in list]
+    return dict(sorted(swapped_list,reverse=True))
 
 def count_words(s):
     s = s.split()
@@ -25,6 +41,14 @@ def count_unique(s):
         else:
             character_count[char] = 1
         
-    print(character_count)
-            
+    return character_count
+
+def report(path,number_of_words,u_char_count):
+    print(f"--- Begin report of {path} ---")
+    print(f"{number_of_words} words found in the document")
+    for ch in u_char_count:
+        print(f"The {ch} character was found {u_char_count[ch]} times.")
+    print("--- End report ---")
+
+
 main()
