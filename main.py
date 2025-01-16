fef5fc9eca93e69830ebb01897f64a4ebe003d91
Edesak <siegel.marek@gmail.com>
2025-01-16 20:16:43 +0100

    added unique characters count

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     path = "books/frankenstein.txt"
     text = get_text(path)
     number_of_words = count_words(text)
+    text_lowered = text.lower()
     print(number_of_words)
+
+    count_unique(text_lowered)
 
 def count_words(s):
     s = s.split()
@@ -12,4 +15,16 @@ def get_text(path):
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return(file_contents)
+
+def count_unique(s):
+    characters = set(s)
+    character_count = {}
+    for char in s:
+        if char in character_count:
+            character_count[char] =character_count[char] + 1
+        else:
+            character_count[char] = 1
+        
+    print(character_count)
+            
 main()
